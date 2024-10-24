c20104f329fa721549481c62e4cc401c1fbde795
ghilger <ghilger16@yahoo.com>
2024-10-23 17:05:06 -0700

    init commit

---
+++ src/main/java/com/listblitz/listblitz_microservice/ListblitzMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.listblitz.listblitz_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListblitzMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListblitzMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listblitz/listblitz_microservice/ListblitzMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listblitz.listblitz_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListblitzMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
